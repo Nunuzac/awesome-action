2999d668c2f6b1b08d91337332cab247a851a26a
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-08-18 16:54:21 +0900

    Added action stuff

--- src/index.js
+++ src/index.js
@@ -19,13 +19,10 @@ const run = async () => {
             core.warning(`Wrong format of ad-hoc replacement "${replacement}"`);
         }
     });
+    let out = data;
     map.forEach((key, val) => {
-        console.log(`${key} => ${val}`);
-        console.log(data.replace(key, val));
-        console.log('********');
-        console.log(data.replace(val, key));
-        console.log('********');
-        data = data.replace(key, val);
+        out = out.replace(val, key);
+        console.log(out);
     });
     fs.writeFileSync(path.resolve(outputFile), data);
     core.setOutput('template-output-path', outputFile);
