3f12b7d81550dc4b0264dd283759f5bb0589f3df
Johann Guayacundo Avellaneda <johann.guayacundo@rapyuta-robotics.com>
2022-08-19 17:52:16 +0900

    :tada:

---
+++ script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+IFS=$'\n'
+lines=($REPL)
+tmp=$(cat $1)
+for l in "${lines[@]}"
+do
+    key=$(echo $l | sed -e "s/\(.\+\)=\".\+\"/\1/g")
+    val=$(echo $l | sed -e "s/.\+=\"\(.\+\)\"/\1/g")
+    tmp=$(sed -e "s~%$key~$val~g" <<< $tmp)
+done
+echo $REPL
+exit 1
